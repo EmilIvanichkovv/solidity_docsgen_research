5ec19323424d645804f2fcd3b69a92344895a9d4
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-06-10 15:51:35 +0300

    chore(): A bunch of changes

--- libs/contracts/contracts/example.sol
+++ libs/contracts/contracts/example.sol
@@ -6,6 +6,12 @@ pragma solidity ^0.8.0;
  * @dev A simple smart contract to demonstrate inline documentation
  */
 contract MyContract {
+    /// @notice Mask for getFeedById(uint32 key)
+    /// @dev The maximum length of the key is defined by the mask from the selector.
+    /// If the selector is less than the mask, then the function is a getter and the selector holds the key.
+    bytes32 internal constant CONTRACT_MANAGEMENT_SELECTOR =
+      0x000000000000000000000000000000000000000000000000000000000001ffff;
+
     /// A public variable to store a number
     uint256 public myNumber;
 
@@ -36,4 +42,29 @@ contract MyContract {
     function multiplyNumbers(uint256 _a, uint256 _b) external pure returns (uint256) {
         return _a * _b;
     }
+
+    /**
+     * @dev Deposits a public key
+     * @param pubkey The public key to deposit
+     */
+    event Deposited(bytes pubkey);
+
+    // custom error
+    /// @notice Error indicating an insufficient balance
+    /// @param balance The current balance of the contract
+    /// @param withdrawAmount The requested withdrawal amount
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    /// @notice Tests the custom error by attempting to withdraw an amount
+    /// @param _withdrawAmount The amount to withdraw
+    function testCustomError(uint256 _withdrawAmount) public view {
+        uint256 bal = address(this).balance;
+        if (bal < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: bal,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+    }
+
 }
--- libs/contracts/docs/solidity-docgen/example.md
+++ libs/contracts/docs/solidity-docgen/example.md
@@ -4,6 +4,17 @@
 
 _A simple smart contract to demonstrate inline documentation_
 
+### CONTRACT_MANAGEMENT_SELECTOR
+
+```solidity
+bytes32 CONTRACT_MANAGEMENT_SELECTOR
+```
+
+Mask for getFeedById(uint32 key)
+
+_The maximum length of the key is defined by the mask from the selector.
+If the selector is less than the mask, then the function is a getter and the selector holds the key._
+
 ### myNumber
 
 ```solidity
@@ -67,4 +78,47 @@ _Multiplies two numbers and returns the result_
 | Name | Type | Description |
 | ---- | ---- | ----------- |
 | [0] | uint256 | The product of `_a` and `_b` |
+
+### Deposited
+
+```solidity
+event Deposited(bytes pubkey)
+```
+
+_Deposits a public key_
+
+#### Parameters
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| pubkey | bytes | The public key to deposit |
+
+### InsufficientBalance
+
+```solidity
+error InsufficientBalance(uint256 balance, uint256 withdrawAmount)
+```
+
+Error indicating an insufficient balance
+
+#### Parameters
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| balance | uint256 | The current balance of the contract |
+| withdrawAmount | uint256 | The requested withdrawal amount |
+
+### testCustomError
+
+```solidity
+function testCustomError(uint256 _withdrawAmount) public view
+```
+
+Tests the custom error by attempting to withdraw an amount
+
+#### Parameters
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| _withdrawAmount | uint256 | The amount to withdraw |
 
